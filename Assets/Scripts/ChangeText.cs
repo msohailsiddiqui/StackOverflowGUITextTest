e9ed81e5e24a65a93394d596137b12444f712cd6
Sohail Siddiqui <sohails@gmail.com>
2015-06-15 15:27:02 +0500

    First Commit
    First commit of basic project

---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeText : MonoBehaviour 
+{
+	GUIText racename;
+	public int racenum = 1;
+	
+	void Start()
+	{
+		racename = gameObject.GetComponent<GUIText>();
+		racename.text = "Human";
+	}
+	
+	void Update()
+	{
+		if (racenum == 1)
+		{
+			racename = gameObject.GetComponent<GUIText>();
+			racename.text = "Human";
+		}
+		if (racenum == 2)
+		{
+			racename = gameObject.GetComponent<GUIText>();
+			racename.text = "Elf";
+		}
+	}
+}
